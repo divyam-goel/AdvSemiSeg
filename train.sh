5cb738067617aa54eb300aa204eff45f25c23eda
Divyam Goel <divyamgoel19@gmail.com>
2019-04-09 12:39:42 +0530

    Fix: Created new file train_aug_new with list of all images

--- train.sh
+++ train.sh
@@ -3,4 +3,5 @@ python train.py --snapshot-dir snapshots \
                 --num-steps 20000 \
                 --lambda-adv-pred 0.01 \
                 --lambda-semi 0.1 --semi-start 5000 --mask-T 0.2 \
-                --gpu 1
+                --gpu 1 \
+                --data-list ./dataset/voc_list/train_aug_new.txt
