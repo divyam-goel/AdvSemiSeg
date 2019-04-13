054376b3046c6aa8c55f7d9408f3839a26d72c76
Divyam Goel <divyamgoel19@gmail.com>
2019-04-13 08:58:16 +0530

    Feature: Added evaluate.sh. Changed .gitignore

---
+++ evaluate.sh
@@ -0,0 +1,4 @@
+python evaluate_voc.py --restore-from snapshots/VOC_20000.pth \
+                       --save-dir results_20000 \
+                       --data-list ./dataset/voc_list/train_aug_new.txt \
+                       --gpu 1
